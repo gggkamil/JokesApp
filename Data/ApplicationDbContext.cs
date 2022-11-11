d612188ef701d9dc760ac0831d94d6d298b290f2
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-11 20:51:23 +0100

    Initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokingApp.Models;
+
+namespace JokingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokingApp.Models.Joke> Joke { get; set; }
+    }
+}
