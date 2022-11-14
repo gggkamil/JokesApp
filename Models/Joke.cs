2650131031a400f2e32e0ba23cce8c20c53d5684
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-14 23:51:02 +0100

    Date

--- Models/Joke.cs
+++ Models/Joke.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
-
+        public DateTime Date { get {return DateTime.Now;}}
         public Joke()
         {
 
